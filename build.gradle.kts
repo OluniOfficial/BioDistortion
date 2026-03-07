cc9fab57d8fca84ef612b729793a853b53f26b8c
oluni <oluniofficial@gmail.com>
2026-03-07 17:51:25 +0200

    Improving the Gradle architecture

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,8 @@
 plugins {
-    kotlin("jvm") version "2.3.0"
-    id("com.gradleup.shadow") version "8.3.0"
-    id("xyz.jpenilla.run-paper") version "2.3.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.run.paper)
+    alias(libs.plugins.shadow)
 }
-
-group = "oluni.official"
-version = "1.0-beta"
 
 repositories {
     mavenCentral()
@@ -15,31 +12,25 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("net.kyori:adventure-api:4.26.1")
+    compileOnly(libs.paper.api)
+    implementation(libs.bundles.mccoroutine)
 }
 
 tasks {
     runServer {
         minecraftVersion("1.21.8")
     }
-}
-
-val targetJavaVersion = 21
-kotlin {
-    jvmToolchain(targetJavaVersion)
-}
-
-tasks.build {
-    dependsOn("shadowJar")
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        relocate("kotlin", "oluni.official.libs.kotlin")
+        relocate("kotlinx", "oluni.official.libs.kotlinx")
+        relocate("com.github.shynixn.mccoroutine", "oluni.official.libs.mccoroutine")
     }
 }
+
+kotlin {
+    jvmToolchain(21)
+}
+
